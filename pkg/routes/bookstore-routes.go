6c6c8ae7a2b0b0aead5380ffb04ae85dd2f3c411
Christian Jodra <c.jodra14@gmail.com>
2022-05-26 00:43:55 +0200

    register rename

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var RegisterBookSToreRoutes = func(router *mux.Router) {
+var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
 	router.HandleFunc("/book/{bookID}", controllers.GetBookByID).Methods("GET")
