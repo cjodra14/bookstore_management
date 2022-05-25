429f2fccd78350ee266ae70546706b98bd1535a0
Christian Jodra <c.jodra14@gmail.com>
2022-05-26 00:44:23 +0200

    main first steps (router and run server)

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/cjodra14/bookstore_management/pkg/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(router)
+	http.Handle("/", router)
+	log.Fatal(http.ListenAndServe("localhost:9010", router))
 }
